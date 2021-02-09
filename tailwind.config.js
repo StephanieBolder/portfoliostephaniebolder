7ed8ff406b7f903de67ff3a86d90cf813a1fc1a0
bolderstephanie <bolderstephanie@gmail.com>
2021-02-09 10:50:25 +0100

    Responsive Header

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      rood: '#F7F7F7' ,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
+  }
+}
